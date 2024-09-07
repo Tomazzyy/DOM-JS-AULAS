70748b8cf200d9a4486bd6542ad43d990ceb0400
Tomazzyy <viniciustomaz654@gmail.com>
2024-09-06 23:34:01 -0300

    APRENDENDO DOM

---
+++ aula01/script.js
@@ -0,0 +1,12 @@
+document.title = "Javascript - DOM ALTERADO";
+
+//mudando background
+document.body.style.background = "red";
+//document.body.style.backgroundImage = "url('OIP.jpeg')";
+
+
+//escrever na página
+document.write("<h1>Escrevendo titulo pelo JS</h1>");
+document.write("<p>Escrevendo paragrafo pelo JS</p");
+document.write("<h1>Escrevendo titulo pelo JS e <br> pulando linhas</h1>");
+
