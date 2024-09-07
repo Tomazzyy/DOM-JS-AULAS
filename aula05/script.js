3dff55887f33717ef563e13d0e04cee8b6990148
Tomazzyy <viniciustomaz654@gmail.com>
2024-09-07 17:20:04 -0300

    tag name

--- aula05/script.js
+++ aula05/script.js
@@ -1,10 +1,1 @@
-document.getElementsByClassName('classe')[1].innerHTML = "Conteúdo do INNER";
-document.getElementsByClassName('classe')[0].innerHTML = "Conteúdo do INNER";
-
-setTimeout(function(){
-
-    document.getElementsByClassName('classe')[0].innerHTML = "Conteúdo mudado em 3 segundos";
-
-
-
-}, 3000);
+document.getElementsByTagName('h1')[1].innerHTML = "elemento por tag name";
